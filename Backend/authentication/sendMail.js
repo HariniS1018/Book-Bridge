4baff081f85b5322a051d3896c5b1d3d984b1f64
HariniS1018 <hariniks0618@gmail.com>
2025-09-30 22:24:45 +0530

    feat: added mail sending code but need to integrate with auth

---
+++ Backend/authentication/sendMail.js
@@ -0,0 +1,53 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+// Step 1: Create a transporter (the mailman)
+// its indirect version of saying 
+// host: "smtp.google.com" (Gmail's SMTP server, use when u need full ctrl over SMTP settings), 
+// port: 587 ( 587 for STARTLS - secure connection after handshake, 465 for SSL/TLS - secure from the start ), 
+// secure: false (tells nodemailer whether to use SSL/TLS encryption immediately, 'false' means: 'Start with plain connection, then upgrade to secure (STARTTLS)'.)
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+await transporter.verify();
+console.log("Server is ready to take our messages");
+
+// Step 2: generate OTP
+const generateOTP = () =>
+  Math.floor(100000 + Math.random() * 900000).toString();
+const otp = generateOTP();
+
+// Step 3: Define the mail
+const mailOptions = {
+  from: process.env.EMAIL_USER,
+  to: process.env.CLIENT_EMAIL,
+  subject: "OTP from BookBridge! Enjoy Reading!",
+  text: `Hi there! Your OTP is ${otp}. It is valid for only 5 minutes.`,
+  html: `
+      <div style="font-family: Arial, sans-serif; padding: 20px; border: 1px solid #eee;">
+        <h2 style="color: #4CAF50;">BookBridge Email Verification</h2>
+        <p>Hi there!</p>
+        <p>Your OTP is:</p>
+        <h1 style="background: #f0f0f0; padding: 10px; display: inline-block;">${otp}</h1>
+        <p>This code is valid for only <strong>5 minutes</strong>.</p>
+        <p>Enjoy reading and sharing with BookBridge!</p>
+      </div>
+    `,
+};
+
+// Step 4: Send the mail
+(async () => {
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Message sent: %s", info.messageId);
+  } catch (err) {
+    console.error("Error while sending mail", err);
+  }
+})();
