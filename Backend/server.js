792f705157d3cf56e4085ccc2f9acc026c053914
HariniS1018 <hariniks0618@gmail.com>
2025-10-05 14:49:12 +0530

    docs: added docs of code details

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,9 @@
 import app from "./App.js";
 import { sequelize } from "./db/sequelize.js"
 import models from "./models/index.js";
-import { getPool, closePool } from "./db/dbUtils.js";
 
 const PORT = process.env.PORT || 3000;
 
-getPool();
 
 sequelize
   .authenticate()
@@ -25,26 +23,34 @@ sequelize
 sequelize.models = models;
 
 // Graceful shutdown handlers
-process.on("SIGINT", () => {
+
+//- Triggered when you press Ctrl+C in the terminal. - Closes Sequelize’s DB connection pool. - Exits with code 0 (success).
+process.on("SIGINT", async () => {
   console.log("SIGINT signal received. Shutting down gracefully...");
-  closePool();
+  await sequelize.close();
   process.exit(0);
 });
 
-process.on("SIGTERM", () => {
+// - Triggered when the OS or a process manager (like Docker or PM2) tells your app to shut down.
+// - Closes Sequelize’s DB connection pool. - Exits with code 0 (success).
+process.on("SIGTERM", async () => {
   console.log("SIGTERM signal received. Shutting down gracefully...");
-  closePool();
+  await sequelize.close();
   process.exit(0);
 });
 
+// - Triggered when your code throws an error that’s not caught anywhere.
+// - Logs the error and stack trace. - Closes DB connections and exits with code 1 (failure).
 process.on("uncaughtException", async (err) => {
   console.error("Uncaught Exception:", err.message, err.stack);
-  await closePool();
+  await sequelize.close();
   process.exit(1);
 });
 
+// - Triggered when a Promise is rejected but not caught with .catch() or try/catch.
+// - Logs the reason and promise. - Cleans up and exits.
 process.on("unhandledRejection", async (reason, promise) => {
   console.error("Unhandled Rejection at:", promise, "reason:", reason);
-  await closePool();
+  await sequelize.close();
   process.exit(1);
 });
