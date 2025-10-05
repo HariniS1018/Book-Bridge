ee0e24b4779d4a8fb43b5fd5d9f55e1b1fec8400
HariniS1018 <hariniks0618@gmail.com>
2025-10-05 14:49:58 +0530

    docs: added docs of code details and removed unused file

--- Backend/middlewares/errorHandler.js
+++ Backend/middlewares/errorHandler.js
@@ -1,13 +1,13 @@
 const errorHandler = (err, req, res, next) => {
   console.error("[Global Error Handler] Caught an error:", err);
 
-  const statusCode = err.statusCode || 500;
+  const statusCode = err.statusCode || 500;   // sets statuscode from the error object or defaults to 500
   let errorMessage = "Internal Server Error";
   let errorStack = null;
 
   if (process.env.NODE_ENV === "development") {
     errorMessage = err.message;
-    errorStack = err.stack;
+    errorStack = err.stack; // holds stack trace in development mode
   } else {
     errorMessage =
       statusCode === 500
